d0ffb0bc0711a8500112a7f619db1fbce7a40b25
EduardoHeitor <eduardohpmp@gmail.com>
2025-09-02 22:40:25 -0300

    cupido

---
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Cupido.java
@@ -0,0 +1,27 @@
+import br.edu.insper.desagil.aps3.tindfy.Usuario;
+
+import java.util.*;
+
+public class Cupido {
+    private Map<Integer, List<Integer>> likes;
+
+    public Cupido() {
+        this.likes = new HashMap<>();
+    }
+
+    public void daLike(int id1, int id2) {
+        likes.putIfAbsent(id1, new ArrayList<>());
+        List<Integer> lista = likes.get(id1);
+        if (!lista.contains(id2)) {
+            lista.add(id2);
+        }
+    }
+
+    public boolean deuMatch(Usuario u1, Usuario u2) {
+        int id1 = u1.getID();
+        int id2 = u2.getID();
+
+        return likes.containsKey(id1) && likes.get(id1).contains(id2) &&
+                likes.containsKey(id2) && likes.get(id2).contains(id1);
+    }
+}
--- tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Usuario.java
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Usuario.java
@@ -1,7 +1,37 @@
 package br.edu.insper.desagil.aps3.tindfy;
 
-public class Usuario {
 
+import java.util.ArrayList;
+import java.util.List;
 
+public class Usuario extends Pessoa {
 
+    private final List<Musica> musicas;
+
+    // Construtor NÃO recebe música: a lista começa vazia
+    public Usuario(int id, String nome) {
+        super(id, nome);
+        this.musicas = new ArrayList<>();
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    // verifica se já existe música do mesmo ARTISTA (comparando id) e mesmo TÍTULO (equals)
+    public boolean jaAdicionou(Musica musica) {
+        for (Musica m : musicas) {
+            if (m.getArtista().getID() == musica.getArtista().getID()
+                    && m.getTitulo().equals(musica.getTitulo())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void adiciona(Musica musica) {
+        if (!jaAdicionou(musica)) {
+            musicas.add(musica);
+        }
+    }
 }
